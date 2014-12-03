6dfacea7c98ecaa7d4a781a75ad5b3dc5a7aa972
Michael Washburn Jr <mdw7326@g.rit.edu>
2014-12-02 22:03:29 -0500

    fixed tabs

--- mysite/models.py
+++ mysite/models.py
@@ -12,37 +12,37 @@ Project Model represents an individual project which has a title,
 description, images, links, and supporting files.
 """
 class Project(models.Model):
-	title = models.CharField(max_length=200);
-	description = models.TextField();
-	date_posted = models.DateField('Date Posted');
+    title = models.CharField(max_length=200);
+    description = models.TextField();
+    date_posted = models.DateField('Date Posted');
 
-	def __str__(self):
-		return self.title;
+    def __str__(self):
+        return self.title;
 
 """
 Link entry for a Project.
 """
 class ProjectLink(models.Model):
-	project = models.ForeignKey(Project, related_name="ProjectLinks");
-	url = models.CharField(max_length=200);
-	text = models.CharField(max_length=80);
+    project = models.ForeignKey(Project, related_name="ProjectLinks");
+    text = models.CharField(max_length=80);
+    url = models.CharField(max_length=200);
 
-	def __str__(self):
-		return self.pFile.url;
+    def __str__(self):
+        return self.pFile.url;
 
 
 """
 File entry for a Project.
 """
 class ProjectFile(models.Model):
-	project = models.ForeignKey(Project, related_name="ProjectFiles");
-	pFile = models.FileField("File",upload_to=GetUploadSubPath);
-	text = models.CharField(max_length=80);
+    project = models.ForeignKey(Project, related_name="ProjectFiles");
+    text = models.CharField(max_length=80);
+    pFile = models.FileField("File",upload_to=GetUploadSubPath);
 
-	def __str__(self):
-		return self.pFile.url;
+    def __str__(self):
+        return self.pFile.url;
 
-	def GetUrl(self):
-		bad_url = self.pFile.url;
-		url = bad_url[bad_url.find("/media"):];
-		return url;
+    def GetUrl(self):
+        bad_url = self.pFile.url;
+        url = bad_url[bad_url.find("/media"):];
+        return url;
