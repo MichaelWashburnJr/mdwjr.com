9928525f0cab2a872b8e6ec64a175939d1876371
Michael Washburn Jr <mdw7326@g.rit.edu>
2014-12-03 20:10:45 -0500

    removed / to try and fix bad request error

--- mysite/utils.py
+++ mysite/utils.py
@@ -12,5 +12,5 @@ Returns a sub path to the upload_to function of the FileField Model
 field.
 """
 def GetUploadSubPath(instance, filename):
-	return ("/django_project/mysite/media/" + instance.project.title + '/' + filename)
+	return ("django_project/mysite/media/" + instance.project.title + '/' + filename)
 
