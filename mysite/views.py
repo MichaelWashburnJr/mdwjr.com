47c11a86c376ec6b0ef8c7577504c22b689a78fe
Michael D. Washburn Jr <mdw7326@g.rit.edu>
2014-12-06 19:37:54 -0500

    Undid the order reversing. It was a mistake. I didnt restart the server last time.

--- mysite/views.py
+++ mysite/views.py
@@ -48,7 +48,7 @@ def project(request, project_id):
 A list view of all the projects.
 """
 def projects(request):
-	projects = Project.objects.all().order_by("-date_posted", "-id").reverse();
+	projects = Project.objects.all().order_by("-date_posted", "-id")
 	context = {
 		'request' : request,
 		'projects' : projects
