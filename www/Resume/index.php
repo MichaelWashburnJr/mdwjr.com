afd13b9a317461195aec9c6ba9d5f18fbf3f14c8
Michael Washburn Jr <mdw7326@rit.edu>
2015-03-18 14:56:24 -0400

    Addicently deleted instead of rename

---
+++ www/Resume/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Michael Washburn Jr</title>
+    <meta name="description" content=""/>
+    <meta name="keywords" content="Micael Washburn Jr Software Engineer RIT Rochester Institute Technology"/>
+    <?php include('/var/www/mdwjr.com/www/head_tags.php');?>
+ 
+</head>
+
+<?php include('/var/www/mdwjr.com/www/nav.php');?>
+ 
+<body id="">
+
+
+    <div class="container">
+        
+        <!-- CUSTOM CONTENT GOES HERE -->
+
+        <iframe src="https://docs.google.com/document/d/1LLIdJ6BHghbl3XWSMex09vz34i3IHAHnfvEQ1U85eJo/pub?embedded=true"></iframe>
+
+        <!-- END CUSTOM CONTENT -->
+
+    </div>
+ 
+    <?php include('/var/www/mdwjr.com/www/foot_tags.php');?>
+
+</body>
+ 
+</html>
+
