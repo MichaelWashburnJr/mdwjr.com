6fb9276d048ae139102a0979df58ad016dbf4aae
Michael Washburn Jr <mdw7326@rit.edu>
2015-03-18 15:39:58 -0400

    Add contact page

--- www/Resume/index.php
+++ www/Resume/index.php
@@ -16,18 +16,10 @@
     <div class="container">
         
         <!-- CUSTOM CONTENT GOES HERE -->
-        <div class="panel panel-primary">
-            <div class="panel-heading">
 
-                <h3 class="panel-title">My Résumé</h3>
-
-            </div>
-            <div class="panel-body">
                 <iframe src="https://drive.google.com/a/g.rit.edu/file/d/0BwEjRillyZroTVlQd3BHdkxaV3M/preview" width="100%" height="1068px;"></iframe>
                 Download the pdf <a href="https://drive.google.com/a/g.rit.edu/file/d/0BwEjRillyZroTVlQd3BHdkxaV3M/preview">here</a>.
 
-            </div>
-        </div>
         <!-- END CUSTOM CONTENT -->
 
     </div>
