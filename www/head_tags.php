5dcaf75956bd83b076a23783940f03f2ba3a263f
Michael Washburn Jr <mdw7326@rit.edu>
2015-03-18 14:36:06 -0400

    Fix tags in templates

--- www/head_tags.php
+++ www/head_tags.php
@@ -3,13 +3,13 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <!--bootstrap-->
-<link rel="stylesheet" type="text/css" href="/var/www/mdwjr.com/www/rsc/css/bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="/rsc/css/bootstrap.min.css"/>
 <!--bootstrap-theme-->
-<link rel="stylesheet" type="text/css" href="/var/www/mdwjr.com/www/rsc/css/bootstrap-theme.css"/>
+<link rel="stylesheet" type="text/css" href="/rsc/css/bootstrap-theme.css"/>
 
 <!--Additional CSS-->
-<link rel="stylesheet" type="text/css" href="/var/www/mdwjr.com/www/rsc/css/additional.css"/>
+<link rel="stylesheet" type="text/css" href="/rsc/css/additional.css"/>
 
 <!--Favicon-->
-<link rel="shortcut icon" href="/var/www/mdwjr.com/www/rsc/img/favicon.ico" type="image/x-icon"/>
-<link rel="icon" href="/var/www/mdwjr.com/www/rsc/img/favicon.ico" type="image/x-icon"/>
+<link rel="shortcut icon" href="/rsc/img/favicon.ico" type="image/x-icon"/>
+<link rel="icon" href="/rsc/img/favicon.ico" type="image/x-icon"/>
