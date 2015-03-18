1ec57fba13fe737758fff899daba5e97bbe9a764
Michael Washburn Jr <mdw7326@rit.edu>
2015-03-18 15:45:03 -0400

    Add panel body to contact page

--- www/Contact/index.php
+++ www/Contact/index.php
@@ -21,27 +21,31 @@
                 <h3 class="panel-title">Contact Information</h3>
             </div>
 
-            <p>Email: <a href="mailto:mdw7326@rit.edu">mdw7326@rit.edu</a></p>
+            <div class="panel-body">
 
-            <p>LinkedIn:           
-                <a href="http://www.linkedin.com/pub/michael-washburn-jr/78/453/a06">
-                    <img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_myprofile_160x33.png" width="160" height="33" border="0" alt="View Michael Washburn Jr's profile on LinkedIn">
-                </a>
-            </p>
+                <p>Email: <a href="mailto:mdw7326@rit.edu">mdw7326@rit.edu</a></p>
+                <hr/>
+                <p>LinkedIn:           
+                    <a href="http://www.linkedin.com/pub/michael-washburn-jr/78/453/a06">
+                        <img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_myprofile_160x33.png" width="160" height="33" border="0" alt="View Michael Washburn Jr's profile on LinkedIn">
+                    </a>
+                </p>
+                <hr/>
+                <p>Facebook:
+                    <div class="badge-large">
+                        <!-- Facebook Badge START --><a href="https://www.facebook.com/michael.washburnjr" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Michael Washburn Jr">Michael Washburn Jr</a><span style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; line-height: 16px; font-variant: normal; font-style: normal; font-weight: normal; color: #555555; text-decoration: none;">&nbsp;|&nbsp;</span><a href="https://www.facebook.com/badges/" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Make your own badge!">Create Your Badge</a><br/><a href="https://www.facebook.com/michael.washburnjr" target="_TOP" title="Michael Washburn Jr"><img src="https://badge.facebook.com/badge/1825930270.2352.617758974.png" style="border: 0px;" /></a><!-- Facebook Badge END -->
+                    </div>
+                    <div class="badge-small">
+                        <!-- Facebook Badge START --><a href="https://www.facebook.com/michael.washburnjr" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Michael Washburn Jr">Michael Washburn Jr</a><br/><a href="https://www.facebook.com/michael.washburnjr" target="_TOP" title="Michael Washburn Jr"><img src="https://badge.facebook.com/badge/1825930270.3163.2039350056.png" style="border: 0px;" /></a><br/><a href="https://www.facebook.com/badges/" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Make your own badge!">Create Your Badge</a><!-- Facebook Badge END -->
+                    </div>
+                </p>
+                <hr/>
+                <p>Github:
+                    <div class="github-card" data-github="mdw7326" data-width="400" data-height="150" data-theme="default"></div>
+                    <script src="http://lab.lepture.com/github-cards/widget.js"></script>
+                </p>
 
-            <p>Facebook:
-                <div class="badge-large">
-                    <!-- Facebook Badge START --><a href="https://www.facebook.com/michael.washburnjr" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Michael Washburn Jr">Michael Washburn Jr</a><span style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; line-height: 16px; font-variant: normal; font-style: normal; font-weight: normal; color: #555555; text-decoration: none;">&nbsp;|&nbsp;</span><a href="https://www.facebook.com/badges/" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Make your own badge!">Create Your Badge</a><br/><a href="https://www.facebook.com/michael.washburnjr" target="_TOP" title="Michael Washburn Jr"><img src="https://badge.facebook.com/badge/1825930270.2352.617758974.png" style="border: 0px;" /></a><!-- Facebook Badge END -->
-                </div>
-                <div class="badge-small">
-                    <!-- Facebook Badge START --><a href="https://www.facebook.com/michael.washburnjr" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Michael Washburn Jr">Michael Washburn Jr</a><br/><a href="https://www.facebook.com/michael.washburnjr" target="_TOP" title="Michael Washburn Jr"><img src="https://badge.facebook.com/badge/1825930270.3163.2039350056.png" style="border: 0px;" /></a><br/><a href="https://www.facebook.com/badges/" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Make your own badge!">Create Your Badge</a><!-- Facebook Badge END -->
-                </div>
-            </p>
-
-            <p>Github:
-                <div class="github-card" data-github="mdw7326" data-width="400" data-height="150" data-theme="default"></div>
-                <script src="http://lab.lepture.com/github-cards/widget.js"></script>
-            </p>
+            </div>
 
         </div>
         <!-- END CUSTOM CONTENT -->
