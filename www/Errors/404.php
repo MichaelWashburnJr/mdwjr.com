08ab5885f9c0a4285e7d81c136e5ede755bb31c5
Michael Washburn Jr <mdw7326@rit.edu>
2015-03-18 15:54:19 -0400

    Add 404 error page

---
+++ www/Errors/404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Michael Washburn Jr</title>
+    <meta name="description" content=""/>
+    <meta name="keywords" content="Micael Washburn Jr Software Engineer RIT Rochester Institute Technology"/>
+    <?php include('/var/www/mdwjr.com/www/head_tags.php');?>
+ 
+</head>
+
+<?php include('/var/www/mdwjr.com/www/nav.php');?>
+ 
+<body id="">
+
+
+    <div class="container">
+     
+        <!-- CUSTOM CONTENT GOES HERE -->
+        <div class="jumbotron">
+        	<h1>Error 404, Page Not Found</h1>
+        </div>
+        <!-- END CUSTOM CONTENT -->
+
+    </div>
+ 
+    <?php include('/var/www/mdwjr.com/www/foot_tags.php');?>
+
+</body>
+ 
+</html>
