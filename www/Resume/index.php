4848be1ac5b11d805ea9c0f4f1efc1286696085a
Michael Washburn Jr <mdw7326@rit.edu>
2015-03-18 15:15:53 -0400

    Add embedded google doc

--- www/Resume/index.php
+++ www/Resume/index.php
@@ -16,9 +16,18 @@
     <div class="container">
         
         <!-- CUSTOM CONTENT GOES HERE -->
+        <div class="panel panel-primary">
+            <div class="panel-heading">
 
-        <iframe src="https://docs.google.com/document/d/1LLIdJ6BHghbl3XWSMex09vz34i3IHAHnfvEQ1U85eJo/pub?embedded=true"></iframe>
+                <h3 class="panel-title">My Résumé</h3>
 
+            </div>
+            <div class="panel-body">
+                <iframe src="https://drive.google.com/a/g.rit.edu/file/d/0BwEjRillyZroTVlQd3BHdkxaV3M/preview" width="100%" height="1068px;"></iframe>
+                Download the pdf <a href="https://drive.google.com/a/g.rit.edu/file/d/0BwEjRillyZroTVlQd3BHdkxaV3M/preview">here</a>.
+
+            </div>
+        </div>
         <!-- END CUSTOM CONTENT -->
 
     </div>
