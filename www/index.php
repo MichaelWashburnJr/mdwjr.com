f431334caaadd59c836b4a3e5a5219683f236a0e
Michael Washburn Jr <mdw7326@rit.edu>
2015-04-02 20:51:21 -0400

    Change contact info to be on home page

--- www/index.php
+++ www/index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
     <title>Michael Washburn Jr</title>
     <meta name="description" content="Michael Washburn Jr's Personal Website."/>
     <meta name="keywords" content="Micael Washburn Jr Software Engineer RIT Rochester Institute Technology"/>
-    <?php include('/var/www/mdwjr.com/www/rsc/parts/head_tags.html');?>
+    <?php include('/rsc/parts/head_tags.html');?>
  
 </head>
 
-<?php include('/var/www/mdwjr.com/www/rsc/parts/nav.html');?>
+<?php include('/rsc/parts/nav.html');?>
  
 <body id="home">
 
@@ -36,29 +35,54 @@
                 </span>
             </div>
         </div>
-        
 
-        <div class="panel panel-primary">
-            <div class="panel-heading">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
 
-                <h3 class="panel-title">About Me:</h3>
+            <h3 class="panel-title">About Me:</h3>
 
-            </div>
-            <div class="panel-body">
-                I am going into my 3<sup>rd</sup> year at RIT, majoring in Software Engineering.
-                I'm currently working as a Course Assistant for a Math Models of Software Engineering class.
-                Outside of classes, I like to work on my own projects as much as possible.  My interests
-                in software range from Web Development to embedded software (Arduino's are fun!).
-                You can view projects of mine <a href="./Projects">here</a>, and I would also encourage
-                you to check out my <a href="https://github.com/mdw7326" target="_blank">Github Account</a>.
-                Additionally, I enjoy to go hunting, fishing, snowmobiling, and watch Netflix.
-                If you wish to get a hold of me, visit my <a href="./Contact/">Contact page</a>.
-            </div>
         </div>
-
+        <div class="panel-body">
+            I am going into my 3<sup>rd</sup> year at RIT, majoring in Software Engineering.
+            I'm currently working as a Course Assistant for a Math Models of Software Engineering class.
+            Outside of classes, I like to work on my own projects as much as possible.  My interests
+            in software range from Web Development to embedded software (Arduino's are fun!).
+            You can view projects of mine <a href="./Projects">here</a>, and I would also encourage
+            you to check out my <a href="https://github.com/mdw7326" target="_blank">Github Account</a>.
+            Additionally, I enjoy to go hunting, fishing, snowmobiling, and watch Netflix.
+            If you wish to get a hold of me, visit my <a href="./Contact/">Contact page</a>.
+        </div>
     </div>
+
+    <div class="btn-group btn-group-justified">
+        <a class="btn btn-social-icon btn-primary" role="button" href="mailto:mdw7326@rit.edu">
+            <i class="fa fa-envelope-o"></i>
+        </a>
+
+        <a class="btn btn-social-icon btn-linkedin" role="button" target="_blank" href="http://www.linkedin.com/pub/michael-washburn-jr/78/453/a06">
+            <i class="fa fa-linkedin"></i>
+        </a>
+
+        <a class="btn btn-social-icon btn-facebook" role="button" target="_blank" href="https://www.facebook.com/michael.washburnjr">
+            <i class="fa fa-facebook"></i>
+        </a>
+
+        <a class="btn btn-social-icon btn-twitter" role="button" target="_blank" href="https://twitter.com/Mike_Washburn">
+            <i class="fa fa-twitter"></i>
+        </a>
+
+        <a class="btn btn-social-icon btn-google-plus" role="button" target="_blank" href="https://plus.google.com/116038274590759263782">
+            <i class="fa fa-google-plus"></i>
+        </a>
+
+        <a class="btn btn-social-icon btn-github" role="button" target="_blank" href="https://github.com/mdw7326">
+            <i class="fa fa-github"></i>
+        </a>
+    </div>
+
+
  
-    <?php include('/var/www/mdwjr.com/www/rsc/parts/foot_tags.html');?>
+    <?php include('/rsc/parts/foot_tags.html');?>
 
 </body>
  
