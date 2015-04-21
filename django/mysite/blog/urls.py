d698e05fddc95febbd2d6ffe95d1da65f91ab92e
Michael Washburn Jr <mdw7326@rit.edu>
2015-04-21 08:07:06 -0400

    Changed nav urls and made contact bar smaller

--- django/mysite/blog/urls.py
+++ django/mysite/blog/urls.py
@@ -3,7 +3,7 @@ import blog.views as views
 
 urlpatterns = patterns('',
     url(r'^$', views.post_list, name='home'),
-    url(r'^list$', views.post_list, name='list'),
-    url(r'^(?P<slug>[\w-]+)$', views.post_info, name='post'),
-    url(r'^projects$', views.project_list, name='project_list')
+    url(r'^/list$', views.post_list, name='list'),
+    url(r'^/projects$', views.project_list, name='project_list'),
+    url(r'^/(?P<slug>[\w-]+)$', views.post_info, name='post')
 )
