555f8a9d205f8888368632d16283cff92fb4f58b
Michael Washburn Jr <mdw7326@rit.edu>
2015-05-14 19:35:49 -0400

    Add/Edit content forms now working

--- mysite/content_manager/views.py
+++ mysite/content_manager/views.py
@@ -6,6 +6,9 @@ from .forms import PageForm, ContentForm
 
 @staff_required
 def create_page(request):
+    """
+    Create a new page.
+    """
     if request.method == "POST":
         page_form = PageForm(request.POST)
 
@@ -19,11 +22,10 @@ def create_page(request):
     return render(request, 'cm_create_page.html', {'page_form':page_form})
 
 @staff_required
-def create_content(request):
-    pass
-
-@staff_required
 def edit_page(request, page_id=0):
+    """
+    Edit an existing page. Also provide the option to delete the page.
+    """
     page = get_object_or_404(Page, id=page_id)
     if request.method == "POST":
         page_form = PageForm(request.POST, instance=page)
@@ -36,13 +38,47 @@ def edit_page(request, page_id=0):
             return redirect('cm:panel')
 
         else:
-            return render(request, 'cm_edit_page.html', {'page_form' : page_form, 'failure':True})
+            return render(request, 'cm_edit_page.html', {'page_form' : page_form, 'id' : page.id, 'failure':True})
     page_form = PageForm(instance=page)
     return render(request, 'cm_edit_page.html', {'page_form':page_form, 'id' : page.id})
 
 @staff_required
+def create_content(request):
+    """
+    Create a new content object.
+    """
+    if request.method == "POST":
+        content_form = ContentForm(request.POST)
+
+        if content_form.is_valid():
+            content = content_form.save()
+            return redirect('cm:panel')
+
+        else:
+            return render(request, 'cm_create_content.html', {'form' : content_form, 'failure':True})
+    content_form = ContentForm()
+    return render(request, 'cm_create_content.html', {'form':content_form})
+
+@staff_required
 def edit_content(request, content_id=0):
-    pass
+    """
+    Edit an existing content object.
+    """
+    content = get_object_or_404(Content, id=content_id)
+    if request.method == "POST":
+        content_form = ContentForm(request.POST, instance=content)
+
+        if content_form.is_valid():
+            if 'submit' in request.POST:
+                content = content_form.save()
+            elif 'delete' in request.POST:
+                content.delete()
+            return redirect('cm:panel')
+
+        else:
+            return render(request, 'cm_edit_content.html', {'form' : content_form, 'id':content.id, 'failure':True})
+    content_form = ContentForm(instance=content)
+    return render(request, 'cm_edit_content.html', {'form':content_form, 'id':content.id})
 
 @staff_required
 def panel(request):
