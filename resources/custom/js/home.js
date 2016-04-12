a176278e253282488fa63b244877da89c92161a6
Michael Washburn <mdw7326@rit.edu>
2016-04-11 21:07:42 -0400

    Make landing page scroll after 3 seconds on load

--- resources/custom/js/home.js
+++ resources/custom/js/home.js
@@ -1,5 +1,7 @@
-var scrollToID = function(id){
+function scrollToID(id){
   $('html, body').animate({
       scrollTop: $(id).offset().top
   }, 2000);
 };
+
+setTimeout(scrollToID.bind(null, '#profile'), 3000);
